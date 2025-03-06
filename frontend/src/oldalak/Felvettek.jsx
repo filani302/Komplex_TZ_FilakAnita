8c72cef899f1b155eb109a77ea9daf86facb6981
filani302 <filani302@hengersor.hu>
2025-03-06 12:40:35 +0100

    Felvettek

--- frontend/src/oldalak/Felvettek.jsx
+++ frontend/src/oldalak/Felvettek.jsx
@@ -4,7 +4,52 @@ const Felvettek = (() =>{
 
     return(
         <div>
-            <h1>felvettek</h1>
+            <div className="container2">
+
+            <div className="kettes barna">
+
+            <h3>Központi felvételi tájékoztató</h3>
+            <p>A középiskolákba történő jelentkezés során az iskolák határozzák meg, hogy a felvételi rangsort mi alapján döntik el. A Jószakma Szakgimnázium a felvételi során az általános iskolából hozott és a központi felvételin szerzett pontok alapján rangsorolja az iskolába jelentkezőket.</p>
+            <a id="link" href="https://www.oktatas.hu/kozneveles/kozepfoku_felveteli_eljaras/kozepfoku_felveteli_eljaras_informacioi">Tájékoztató oldal</a>
+            <br />
+            <img id="bagoly" src="/public/logo.png" alt="" />
+
+            </div>
+
+            <div className="kettes">
+
+                <h1>Tájékoztatás</h1>
+                <h3>Jószakma Szakgimnázium</h3>
+                <p>A központi felvételit magyar nyelv és irodalom, illetve matematika tantárgyakból írják a jelentkezők. Mindkét tárgy esetén legfeljebb 50 pont szerezhető. A felvételiző hozott pontjait az általános iskolai év végi eredményei alapján számolják, ez a pontszám legfeljebb 50 pont lehet. A hozott pontokat duplázzák. A központi felvételin szerzett és a hozott pontok összege adja a felvételiző összesített pontszámát.
+                </p>
+                <img src="/public/e-mail-marketing-2745489__340.jpg" alt="" />
+
+                </div>
+
+
+            </div>
+
+            <div>
+                <h1></h1>
+                <p></p>
+                <table>
+                    <thead>
+                        <th>
+                            <tr></tr>
+                            <tr></tr>
+                            <tr></tr>
+                        </th>
+                    </thead>
+                    <tbody>
+                        <th>
+                            <tr></tr>
+                            <tr></tr>
+                            <tr></tr>
+                        </th>
+                    </tbody>
+                </table>
+            </div>
+                
         </div>
     )
 
--- frontend/src/oldalak/Home.jsx
+++ frontend/src/oldalak/Home.jsx
@@ -3,62 +3,88 @@ import React from "react";
 const Home = (() =>{
 
     return(
-        <div className="container">
-            
-            <div className="harmas barna">
-                <h3>Központi felvételi tájékoztató</h3>
-                <p></p>
-                <img src="/public/logo.png" alt="" />
-            </div>
+        
+        <div>
+          
+          <div className="container">
 
-            <div className="harmas">
+                <div className="harmas barna">
+
+                <h3>Központi felvételi tájékoztató</h3>
+                <p>A középiskolákba történő jelentkezés során az iskolák határozzák meg, hogy a felvételi rangsort mi alapján döntik el. A Jószakma Szakgimnázium a felvételi során az általános iskolából hozott és a központi felvételin szerzett pontok alapján rangsorolja az iskolába jelentkezőket.</p>
+                <a id="link" href="https://www.oktatas.hu/kozneveles/kozepfoku_felveteli_eljaras/kozepfoku_felveteli_eljaras_informacioi">Tájékoztató oldal</a>
+                <br />
+                <img id="bagoly" src="/public/logo.png" alt="" />
+
+                </div>
+
+                <div className="harmas">
+
                 <h1>Tájékoztatás</h1>
                 <h3>Jószakma Szakgimnázium</h3>
-                <p></p>
+                <p>A központi felvételit magyar nyelv és irodalom, illetve matematika tantárgyakból írják a jelentkezők. Mindkét tárgy esetén legfeljebb 50 pont szerezhető. A felvételiző hozott pontjait az általános iskolai év végi eredményei alapján számolják, ez a pontszám legfeljebb 50 pont lehet. A hozott pontokat duplázzák. A központi felvételin szerzett és a hozott pontok összege adja a felvételiző összesített pontszámát.
+                </p>
                 <img src="/public/e-mail-marketing-2745489__340.jpg" alt="" />
-            </div>
 
-            <div className="harmas barna">
+                </div>
+
+
+                <div className="harmas barna">
+
                 <h3>Az oldal használatáról</h3>
                 <p>Ön az oldal használatával a következő információkhoz juthat hozzá</p>
-                <div><p>Előzetes rangsor:</p></div>
-                <div>
-                    <div><p>Nevek</p></div>
-                    <div><p>Ágazat</p></div>
-                    <div><p>Összes pontszam</p></div>
+                <div><p>Előzetes rangsor:</p>
+
                 </div>
+
+                <div>
+
+                <div><p>Nevek</p><div/>
+                <div><p>Ágazat</p></div>
+                <div><p>Összes pontszam</p></div>
+
+                </div>
+
                 <div><p>Előzetes rangsor nyelvi előkészítő</p></div>
                 <div><p>A felvettek névsora</p></div>
+                </div>   
+
+           
+            </div>
+        
+            <div id="tablazat">
+                <div>
+                    <h3></h3>
+                    <p></p>
+                    <select name="" id="">
+                        <option value=""></option>
+                    </select>
+                    <button></button>
+                </div>
+                    <p></p>
+                    <table>
+                        <thead>
+                            <th>
+                                <tr></tr>
+                                <tr></tr>
+                                <tr></tr>
+                            </th>
+                        </thead>
+                        <tbody>
+                            <th>
+                                <tr></tr>
+                                <tr></tr>
+                                <tr></tr>
+                            </th>
+                        </tbody>
+                    </table>
+                <div>
+
+                </div>
             </div>
 
-            <div>
-                <h3></h3>
-                <p></p>
-                <Select>
-                    <option value=""></option>
-                </Select>
-                <button></button>
-            </div>
-
-            <div>
-                <table>
-                    <thead>
-                        <th>
-                            <tr></tr>
-                            <tr></tr>
-                            <tr></tr>
-                        </th>
-                    </thead>
-                    <tbody>
-                        <th>
-                            <tr></tr>
-                            <tr></tr>
-                            <tr></tr>
-                        </th>
-                    </tbody>
-                </table>
-            </div>
             
+        </div>
         </div>
     )
 
