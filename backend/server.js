1af8aef9d3555b142aef360c5e1dbf2c28c5429c
filani302 <filani302@hengersor.hu>
2025-03-06 12:58:09 +0100

    slect

--- backend/server.js
+++ backend/server.js
@@ -11,3 +11,24 @@ const db = mysql.createConnection({
     user: "root",
     password: ""
 });
+
+
+
+app.get('tagozatok', (req, res) => {
+    const sqlParancsok = "SELECT * FROM tagozatok ORDER BY agazat";
+    db.query(sqlParancsok, (err, result)=> {
+        if(err){
+            res.json(err);
+        }
+        res.json(result);  
+    })
+})
+
+//SELECT 
+///    d.nev AS "Tanuló neve",
+//    t.agazat AS "Ágazat",
+   // (d.hozott + d.kpmagy + d.kpmat) AS "Összes pontszám"
+//FROM felveteli.diakok d
+//JOIN felveteli.jelentkezesek j ON d.oktazon = j.diak
+//JOIN felveteli.tagozatok t ON j.tag = t.akod
+//ORDER BY "Összes pontszám" DESC;
